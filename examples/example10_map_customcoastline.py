184dcb288fa6c422104edca704ba03caf8980e16
duytung <duytung18793@gmail.com>
2025-10-22 18:22:03 +0700

    update

--- examples/example10_map_customcoastline.py
+++ examples/example10_map_customcoastline.py
@@ -21,10 +21,58 @@ fgrid = Dataset(path + 'grid.nc', 'r')
 lat_t = fgrid.variables['latitude_t'][:]
 lon_t = fgrid.variables['longitude_t'][:]
 
+
+
 # ============================================================
-# EXAMPLE 1: import_4D (time, depth, lat, lon) -> plot level 0, day 0
+# EXAMPLE 10: plot with a custom coast line
 # ============================================================
+
+
+# OpenStreetMap shapefile is in a projected coordinate system (meters) instead of geographic (lat/lon)
+# and Basemap requires longitude–latitude (EPSG:4326) coordinates.
+# So we need to convert it
+# Note: it may be super slow to run this command (5 or more minutes). Please be patient when the code is running. 
+
+import geopandas as gpd
+
+# Read the Web Mercator shapefile
+gdf = gpd.read_file("/data/projects/LOTUS/tungnd/openstreetmap-coastline/lines.shp")
+
+# Convert to geographic coordinates (lon/lat)
+gdf = gdf.to_crs(epsg=4326)
+
+# Save as a new shapefile
+gdf.to_file("/data/projects/LOTUS/tungnd/openstreetmap-coastline/osm_coastline_wgs84.shp")
+
+# Simplified version for faster loading
+shp_simplified = gdf.simplify(tolerance=0.05)
+gdf.geometry = shp_simplified
+gdf.to_file("/data/projects/LOTUS/tungnd/openstreetmap-coastline/osm_coastline_wgs84_simple.shp")
+
+
+# ============================================================
+
+
 sal_full = gc.import_4D(path, 'sal', tstart, tend, ignore_missing='False')
+
+# Plot using the full coastal line
+gc.map_draw(
+    lon_min=105, lon_max=111,
+    lat_min=16.5, lat_max=22,
+    title="Bottom salinity (example 1)",
+    lon_data=lon_t,
+    lat_data=lat_t,
+    data_draw=sal_full[0, 0, :, :],  # day 0, level 0
+    path_save="/prod/projects/data/tungnd/figure/",
+    name_save="example10.1", 
+    custom_coastline = "/data/projects/LOTUS/tungnd/openstreetmap-coastline/osm_coastline_wgs84", 
+    layer_name= "osm_coastline_wgs84",
+)
+
+#Note: it may be super slow to use a custom coastline. Please be patient when the code is running. 
+
+
+# Plot using the simplified coastal line
 
 gc.map_draw(
     lon_min=105, lon_max=111,
@@ -34,8 +82,18 @@ gc.map_draw(
     lat_data=lat_t,
     data_draw=sal_full[0, 0, :, :],  # day 0, level 0
     path_save="/prod/projects/data/tungnd/figure/",
-    name_save="demo_01", 
-    custom_coastline = "/data/projects/LOTUS/tungnd/openstreetmap-coastline/lines", 
-    layer_name= "lines",
+    name_save="example10.2", 
+    custom_coastline = "/data/projects/LOTUS/tungnd/openstreetmap-coastline/osm_coastline_wgs84_simple", 
+    layer_name= "osm_coastline_wgs84_simple",
 )
+
+
+
+
+
+
+
+
+
+
 
